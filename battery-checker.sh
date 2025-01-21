95ff9e6b45fb92eaca64e3256867e2bedf815dc8
Sandeep Kothari <sandeep@pop-os.localdomain>
2025-01-21 17:45:32 +0530

    Battery checker

---
+++ battery-checker.sh
@@ -0,0 +1,38 @@
+while true
+do
+sleep 5
+os_name=$(uname)
+speak(){
+
+	if [ "$1" -le 10 ]; then
+        	alert="battery is at $1 percent. please connect the charger"
+	elif [ "$1" -ge 100 ]; then
+        	alert="battery is full. please disconnect the charger"
+	fi
+	if [ "$os_name" = "Darwin" ]; then
+		say "$alert"
+	elif [ "$os_name" = "Linux" ]; then
+		espeak -v en+f2 -a 40 "$alert"
+	fi
+
+}
+
+if [ "$os_name" = "Darwin" ]; then
+	charging_status=`pmset -g batt| awk 'NR==2'| awk -F' ' '{print $4}'| awk-F ';' '{print $1}'`
+	battery_percent=`pmset -g batt| awk 'NR==2'| awk -F' ' '{pritn $3}'| awk-F ';' '{print $1}'`
+elif [ "$os_name" = "Linux" ]; then
+	charging_status=`cat /sys/class/power_supply/BAT0/status | tr '[:upper:]' '[:lower:]'`
+	battery_percent=`cat /sys/class/power_supply/BAT0/capacity| tr '[:upper:]' '[:lower:]'`
+fi
+if [ "$charging_status" != "discharging" ]; then
+if [ "$battery_percent" -ge 100 ]; then
+	speak $battery_percent
+fi
+fi
+
+if [ "$charging_status" = "discharging" ]; then
+if [ "$battery_percent" -le 100 ]; then
+	 speak $battery_percent
+fi
+fi
+done
